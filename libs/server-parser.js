dc9f863e127926b58ecdc533064bc6cd84494d9e
Michael Scherer <schmic@users.noreply.github.com>
2014-11-27 00:58:51 +0100

    raised maxListener count on merged stream

--- libs/server-parser.js
+++ libs/server-parser.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 module.exports = {
     connect: function (server) {
         var stream = es.merge(server.proc.stdout, server.proc.stderr).pipe(es.split());
+        stream.setMaxListeners(50);
         fs.readdirSync(path.join(__dirname, "parsers")).forEach(function (file) {
             stream.pipe(es.map(require("./parsers/" + file).bind(null, server)));
         });
