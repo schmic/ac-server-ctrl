2edd5f374f2b3f256efa016975eed8ed31d1496f
Michael Scherer <schmic@users.noreply.github.com>
2014-12-06 16:19:09 +0100

    switched to plugin handler

--- libs/server-parser.js
+++ libs/server-parser.js
@@ -2,12 +2,12 @@ var es = require('event-stream');
 var path = require('path');
 var fs = require('fs');
 
-module.exports = {
-    connect: function (server) {
-        var stream = es.merge(server.proc.stdout, server.proc.stderr).pipe(es.split());
-        stream.setMaxListeners(50);
-        fs.readdirSync(path.join(__dirname, "parsers")).forEach(function (file) {
-            stream.pipe(es.map(require("./parsers/" + file).bind(null, server)));
-        });
-    }
+exports.connect = function (server) {
+    var stream = es.merge(server.proc.stdout, server.proc.stderr).pipe(es.split());
+    stream.setMaxListeners(20);
+    stream.pipe(es.map(require("./parsers/file-logger").bind(null, server)));
+    stream.pipe(es.map(require("./parsers/dropper").bind(null, server)));
+    stream.pipe(es.map(require("./parsers/sessions").bind(null, server)));
+    stream.pipe(es.map(require("./parsers/drivers").bind(null, server)));
+    stream.pipe(es.map(require("./parsers/laptimes").bind(null, server)));
 };
