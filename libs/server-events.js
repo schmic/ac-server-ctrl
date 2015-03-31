8c6340e950ce284b92290e02686443d31e78df04
Michael Scherer <schmic@users.noreply.github.com>
2015-03-31 16:40:49 +0200

    added server-events object for event emitting

---
+++ libs/server-events.js
@@ -0,0 +1,24 @@
+module.exports = {
+    server: {
+        start: 'server.start',
+        stop: 'server.stop'
+    },
+    car: {
+        connect: 'car.connect',
+        disconnect: 'car.disconnect'
+    },
+    lap: {
+        time: 'lap.time',
+        best: 'lap.best'
+    },
+    track: {
+        dynamic: 'track.dynamic'
+    },
+    session: {
+        next: 'session.next',
+        end: 'session.end'
+    },
+    race: {
+        end: 'race.end'
+    }
+}
