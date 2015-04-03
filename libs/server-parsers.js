2a75f55c1c0ab19e794422eb77a0928fcd6512b4
Michael Scherer <schmic@users.noreply.github.com>
2015-04-03 15:05:13 +0200

    fix unhandled stream error cause of moved es.split() function

--- libs/server-parsers.js
+++ libs/server-parsers.js
@@ -2,9 +2,8 @@ var es = require('event-stream');
 var acEvents = require('./server-events');
 
 exports.connect = function (server) {
-    var stream = es.merge(server.proc.stdout, server.proc.stderr);
+    var stream = es.merge(server.proc.stdout, server.proc.stderr).pipe(es.split());
     stream.setMaxListeners(20);
-    stream.pipe(es.split());
     stream.pipe(es.map(require("./parsers/file-logger").bind(null, server)));
     stream.pipe(es.map(require("./parsers/dropper").bind(null, server)));
     stream.pipe(es.map(require("./parsers/sessions").bind(null, server)));
